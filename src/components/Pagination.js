d78c44b26481eec912c0b627451427ccad03c006
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2019-05-11 13:59:54 +0200

    Pagination with Hooks

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,70 +1,68 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import PropTypes from "prop-types";
 
-const PageItem = props =>
-    <li className={"page-item" + (props.disabled ? " disabled" : "") + (props.active ? " active" : "")}>
-        <a className="page-link" href={"#" + (props.page || props.children)} aria-label={props.label} onClick={() => props.onClick(props.page)}>
-            {props.label ?
-                <span aria-hidden="true">{props.children}</span>
+const PageItem = ({ disabled, active, page, children, label, onClick }) =>
+    <li className={"page-item" + (disabled ? " disabled" : "") + (active ? " active" : "")}>
+        <a className="page-link" href={"#" + (page || children)} aria-label={label} onClick={() => onClick(page)}>
+            {label ?
+                <span aria-hidden="true">{children}</span>
                 :
-                props.children
+                children
             }
-            {props.active &&
+            {active &&
                 <span className="sr-only">(current)</span>
             }
         </a>
     </li>
 
-class Pagination extends Component {
-    constructor() {
-        super();
 
-        this.state = {
-            currentPage: 1
-        };
-    }
-    getPagesCount = () => {
-        let pages = Math.ceil(this.props.children.length / this.props.itemsPerPage) || 0;
+
+function Pagination(props) {
+    let [currentPage, setCurrentPage] = useState(1);
+
+    function getPagesCount() {
+        let pages = Math.ceil(props.children.length / props.itemsPerPage) || 0;
         return pages;
-    };
-    goToPage = (page) => {
-        this.setState({ currentPage: page });
-    };
-    render() {
-        let children = this.props.children;
-        let start = this.props.itemsPerPage * (this.state.currentPage - 1);
-        let filtered = children.slice(start, start + this.props.itemsPerPage);
-        let view = React.cloneElement(this.props.container, null, filtered);
+    }
 
-        let pageCount = this.getPagesCount();
-        let buttonItems = Array.from(Array(pageCount).keys()).map(i => i + 1);
-        let startIndex = Math.max(0, this.state.currentPage - Math.ceil(this.props.maxButtons / 2));
-        let visibleButtons = Math.min(pageCount, this.props.maxButtons);
-        let endIndex = startIndex + visibleButtons;
-        if (endIndex > pageCount) {
-            startIndex += pageCount - endIndex;
-            endIndex = pageCount;
-        }
-        let buttonsToShow = buttonItems.slice(startIndex, endIndex);
-
+    if (props.children.length == 0) {
         return (
-            <div>
-                {view}
-                <nav aria-label="Page navigation example">
-                    <ul className="pagination justify-content-center">
-                        <PageItem disabled={this.state.currentPage === 1} label="Previous" onClick={this.goToPage} page={this.state.currentPage - 1}>&laquo;</PageItem>
-                        {
-                            buttonsToShow.map((i) =>
-                                <PageItem key={i} active={i === this.state.currentPage} onClick={this.goToPage} page={i}>{i}</PageItem>
-                            )
-                        }
-                        <PageItem disabled={this.state.currentPage === this.getPagesCount()} label="Next" onClick={this.goToPage} page={this.state.currentPage + 1}>&raquo;</PageItem>
-                    </ul>
-                </nav>
-            </div>
-
+            <p>No urls yet</p>
         );
     }
+
+    let start = props.itemsPerPage * (currentPage - 1);
+    let filtered = props.children.slice(start, start + props.itemsPerPage);
+    let view = React.cloneElement(props.container, null, filtered);
+
+    let pageCount = getPagesCount();
+    let buttonItems = Array.from(Array(pageCount).keys()).map(i => i + 1);
+    let startIndex = Math.max(0, currentPage - Math.ceil(props.maxButtons / 2));
+    let visibleButtons = Math.min(pageCount, props.maxButtons);
+    let endIndex = startIndex + visibleButtons;
+    if (endIndex > pageCount) {
+        startIndex += pageCount - endIndex;
+        endIndex = pageCount;
+    }
+    let buttonsToShow = buttonItems.slice(startIndex, endIndex);
+
+    return (
+        <div>
+            {view}
+            <nav aria-label="Page navigation example">
+                <ul className="pagination justify-content-center">
+                    <PageItem disabled={currentPage === 1} label="Previous" onClick={setCurrentPage} page={currentPage - 1}>&laquo;</PageItem>
+                    {
+                        buttonsToShow.map((i) =>
+                            <PageItem key={i} active={i === currentPage} onClick={setCurrentPage} page={i}>{i}</PageItem>
+                        )
+                    }
+                    <PageItem disabled={currentPage === getPagesCount()} label="Next" onClick={setCurrentPage} page={currentPage + 1}>&raquo;</PageItem>
+                </ul>
+            </nav>
+        </div>
+
+    );
 }
 
 Pagination.propTypes = {
