e63abf0d072c32b731466909df41e177469498ee
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2019-05-11 11:35:01 +0200

    Replaced form by Hooks implementation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,53 +1,36 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import IconInput from "./IconInput";
 import { addUrl } from "../actions/urlsActions";
 
 
-function mapDispatchToProps(dispatch) {
-    return {
-        addUrl: todo => dispatch(addUrl(todo))
-    };
-}
+const Form = () => {
+    let [address, setAddress] = useState('');
+    let [alias, setAlias] = useState('');
 
-const mapStateToProps = state => {
-    // Move to selectors.js and use rselect for performance (cache)
-    return { error: state.get("auth").get("error") };
-}
+    const error = useSelector(state => state.get("auth").get("error"))
+    const dispatch = useDispatch();
 
-class ConnectedForm extends Component {
-    constructor() {
-        super();
-        this.state = {
-            address: "",
-            alias: ""
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(fieldName, fieldValue) {
-        this.setState({ [fieldName]: fieldValue });
-    }
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        const { address, alias } = this.state;
-        this.props.addUrl({ address, alias });
-        this.setState({ address: "", alias: "" });
+
+        dispatch(addUrl({ address, alias }));
+        setAddress('');
+        setAlias('');
     }
-    render() {
-        const { address, alias } = this.state;
-        return (
-            <form onSubmit={this.handleSubmit} className="mt-5">
-                <IconInput label="Url" icon="fas fa-globe-europe" type="text" name="address" value={address} onChange={this.handleChange} required={true} autofocus={true} />
-                <IconInput label="Alias" icon="fas fa-bolt" type="text" name="alias" value={alias} onChange={this.handleChange} required={false} />
-                <div className="form-group text-center">
-                    <input type="submit" className="btn btn-primary" value="Create" />
-                </div>
-                <p>{this.props.error}</p>
-            </form>
-        );
-    }
-}
-const Form = connect(mapStateToProps, mapDispatchToProps)(ConnectedForm);
+
+    return (
+        <form onSubmit={handleSubmit} className="mt-5">
+            <IconInput label="Url" icon="fas fa-globe-europe" type="text" name="address" value={address}
+                onChange={(name, value) => setAddress(value)} required={true} autofocus={true} />
+            <IconInput label="Alias" icon="fas fa-bolt" type="text" name="alias" value={alias}
+                onChange={(name, value) => setAlias(value)} required={false} />
+            <div className="form-group text-center">
+                <input type="submit" className="btn btn-primary" value="Create" />
+            </div>
+            <p>{error}</p>
+        </form>
+    )
+};
 export default Form;
